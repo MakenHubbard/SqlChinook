bab24d27a7af42c2f1798b9dc455105fa648cefc
Maken Hubbard <makhub26@outlook.com>
2018-10-16 19:21:56 -0500

    adding the second query that asks for customers just from brazil

--- ChinookQuery1.sql
+++ ChinookQuery1.sql
@@ -2,11 +2,15 @@ use Chinook
 
 /*non_usa_customers.sql: Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.*/
 
--- who are not in the US.
-
 select  
 	[Full Name] = FirstName + ' ' + LastName,
 	[Customer Id] = CustomerId,
 	Country
 from Customer 
 where Country != 'USA'
+
+--brazil_customers.sql: Provide a query only showing the Customers from Brazil.
+
+select *
+from Customer
+where Country = 'brazil'
