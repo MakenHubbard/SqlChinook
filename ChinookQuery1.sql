f16f9a9213ca429b8d3b7df0867c91bacf81e168
Maken Hubbard <makhub26@outlook.com>
2018-10-20 12:18:18 -0500

    adding a query comment

--- ChinookQuery1.sql
+++ ChinookQuery1.sql
@@ -123,4 +123,4 @@ select
 from invoice
 group by invoice.BillingCountry
 
-	
+--playlists_track_count.sql: Provide a query that shows the total number of tracks in each playlist. The Playlist name should be include on the resulant table.
